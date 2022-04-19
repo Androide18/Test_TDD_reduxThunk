4e56efa5845060c8d07f284dd49ff32f73a14042
Androide18 <luciana.ayala1@gmail.com>
2022-04-19 16:37:56 -0300

    first commit

---
+++ src/reducers/usuarios.test.js
@@ -0,0 +1,40 @@
+import { fetchUsuarios } from "./usuarios";
+
+describe("Duck Usuarios", () => {
+  describe("fetches usuarios", () => {
+    it("manejo el caso de exito", async () => {
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+      const services = {
+        axios: {
+          get: jest.fn().mockResolvedValue({
+            data: 1,
+          }),
+        },
+      };
+
+      await fetchUsuarios()(dispatch, getState, services);
+      expect(dispatch.mock.calls).toEqual([
+        [{ type: "FETCH_USUARIOS_START", error: false }],
+        [{ type: "FETCH_USUARIOS_SUCCESS", payload: 1 }],
+      ]);
+    });
+    it("manejo el caso de error", async () => {
+      const dispatch = jest.fn();
+      const getState = jest.fn();
+      const services = {
+        axios: {
+          get: jest.fn().mockRejectedValue(1),
+        },
+      };
+
+      await fetchUsuarios()(dispatch, getState, services);
+      expect(dispatch.mock.calls).toEqual([
+        [{ type: "FETCH_USUARIOS_START", error: false }],
+        [{ type: "FETCH_USUARIOS_ERROR", payload: 1, error: true }],
+      ]);
+    });
+
+
+  });
+});
